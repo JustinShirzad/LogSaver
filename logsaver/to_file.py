f2d646408e79057949748fc6424727330c1cb74a
JustinShirzad <22029504@student.westernsydney.edu.au>
2025-07-12 20:25:45 +1000

    Made LogSaver class for the package

--- logsaver/to_file.py
+++ logsaver/to_file.py
@@ -2,26 +2,37 @@ import os
 import sys
 from datetime import datetime
 
-def create_log_file():
-    relative_path = os.path.join("logs")
+class LogSaver:
+    def __init__(self, log_dir = "logs"):
+        self.log_dir = log_dir
+        self.log_file = self.create_log_file()
 
-    # Creat directory if it doesn't exist
-    os.makedirs(relative_path, exist_ok=True)
+    def create_log_file(self):
+        relative_path = os.path.join(self.log_dir)
 
-    # Get process name
-    process_name = os.path.basename(sys.argv[0])
-    if process_name.endswith(".py"):
-        process_name = process_name[:-3]
+        # Creat directory if it doesn't exist
+        os.makedirs(relative_path, exist_ok=True)
 
-    # Create timestamp
-    timestamp = datetime.now().strftime("%S%M%H_%d-%m-%Y")
+        # Get process name
+        process_name = os.path.basename(sys.argv[0])
+        if process_name.endswith(".py"):
+            process_name = process_name[:-3]
 
-    # Make log file
-    log_filename = f"{process_name}_{timestamp}.log"
-    log_file = os.path.join(relative_path, log_filename)
-    with open(log_file, "w") as log:
-        log.write(f"|=== START OF LOG FOR: {process_name} ON===|\n")
-        log.write(f"|=== Started: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ===|\n")
+        # Create timestamp
+        timestamp = datetime.now().strftime("%S%M%H_%d-%m-%Y")
+
+        # Initialise log file
+        log_filename = f"{process_name}_{timestamp}.log"
+        log_file = os.path.join(relative_path, log_filename)
+        with open(log_file, "w") as log:
+            log.write(f"|=== START OF LOG FOR: {process_name} ===|\n")
+            log.write(f"|=== Started: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ===|\n")
+
+        return log_file
+    
+    def write_to_log(self, message, level="INFO"):
+        # Create timestamp
+        timestamp = datetime.now().strftime("%S%M%H_%d-%m-%Y")
     
 if __name__ == "__main__":
-    create_log_file()
+    logger = LogSaver()
